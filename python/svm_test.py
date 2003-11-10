6006a7100ef86e0f45db2f695d6be9807b8cd368
cjlin <cjlin@c1691e87-3399-dd11-a7e4-0017319806e7>
2003-11-10 06:57:32 +0000

    add the test of decision values

--- python/svm_test.py
+++ python/svm_test.py
@@ -2,14 +2,14 @@
 
 from svm import *
 
-# the XOR problem
-labels = [0, 1, 1, 0]
+# a three-class problem
+labels = [0, 1, 1, 2]
 samples = [[0, 0], [0, 1], [1, 0], [1, 1]]
 problem = svm_problem(labels, samples);
 size = len(samples)
 
-kernels = [LINEAR, POLY, RBF, SIGMOID]
-kname = ['linear','polynomial','rbf','sigmoid']
+kernels = [LINEAR, POLY, RBF]
+kname = ['linear','polynomial','rbf']
 
 param = svm_parameter(C = 10,nr_weight = 2,weight_label = [1,0],weight = [10,1])
 for k in kernels:
@@ -23,3 +23,17 @@ for k in kernels:
 	print "##########################################"
 	print " kernel %s: error rate = %d / %d" % (kname[param.kernel_type], errors, size)
 	print "##########################################"
+
+param = svm_parameter(kernel_type = RBF, C=10)
+model = svm_model(problem, param)
+print "##########################################"
+print " Decision values of predicting %s" % (samples[0])
+print "##########################################"
+
+print "Numer of Classes:", model.get_nr_class()
+d = model.predict_values(samples[0])
+for i in model.get_labels():
+	for j in model.get_labels():
+		if j>i:
+			print "{%d, %d} = %9.5f" % (i, j, d[i,j])
+
