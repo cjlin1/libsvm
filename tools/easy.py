e96acee1dce9cc557e81a96734ec54cedc1f3da5
cjlin <cjlin@c1691e87-3399-dd11-a7e4-0017319806e7>
2003-11-10 06:57:09 +0000

    correct tab

--- tools/easy.py
+++ tools/easy.py
@@ -4,8 +4,8 @@ import sys
 import os
 
 if len(sys.argv) <= 1:
-    print 'Usage: %s training_file [testing_file]' % sys.argv[0]
-    raise SystemExit
+	print 'Usage: %s training_file [testing_file]' % sys.argv[0]
+	raise SystemExit
 
 train_pathname = sys.argv[1]
 assert os.path.exists(train_pathname),"training file not found"
@@ -15,11 +15,11 @@ model_file = file_name + ".model"
 range_file = file_name + ".range"
 
 if len(sys.argv) > 2:
-    test_pathname = sys.argv[2]
-    file_name = os.path.split(test_pathname)[1]
-    assert os.path.exists(test_pathname),"testing file not found"
-    scaled_test_file = file_name + ".scale"
-    predict_test_file = file_name + ".predict"
+	test_pathname = sys.argv[2]
+	file_name = os.path.split(test_pathname)[1]
+	assert os.path.exists(test_pathname),"testing file not found"
+	scaled_test_file = file_name + ".scale"
+	predict_test_file = file_name + ".predict"
 
 cmd = "../svm-scale -s %s %s > %s" % (range_file, train_pathname, scaled_file)
 print 'Scaling training data...'
@@ -31,9 +31,9 @@ dummy, f = os.popen2(cmd)
 
 line = ''
 while 1:
-    last_line = line
-    line = f.readline()
-    if not line: break
+	last_line = line
+	line = f.readline()
+	if not line: break
 c,g,rate = map(float,last_line.split())
 
 print 'Best c=%s, g=%s CV rate=%s' % (c,g,rate)
@@ -44,12 +44,12 @@ os.popen(cmd)
 
 print 'Output model: %s' % model_file
 if len(sys.argv) > 2:
-    cmd = "../svm-scale -r %s %s > %s" % (range_file, test_pathname, scaled_test_file)
-    print 'Scaling testing data...'
-    os.system(cmd)
+	cmd = "../svm-scale -r %s %s > %s" % (range_file, test_pathname, scaled_test_file)
+	print 'Scaling testing data...'
+	os.system(cmd)
 
-    cmd = "../svm-predict %s %s %s" % (scaled_test_file, model_file, predict_test_file)
-    print 'Testing...'
-    os.system(cmd)
+	cmd = "../svm-predict %s %s %s" % (scaled_test_file, model_file, predict_test_file)
+	print 'Testing...'
+	os.system(cmd)
 
-    print 'Output prediction: %s' % predict_test_file
+	print 'Output prediction: %s' % predict_test_file
