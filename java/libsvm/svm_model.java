777e04c1c109688820ad19fe0f20f44fba3482a9
cjlin <cjlin@c1691e87-3399-dd11-a7e4-0017319806e7>
2004-03-18 06:31:08 +0000

    add prob in model

--- java/libsvm/svm_model.java
+++ java/libsvm/svm_model.java
@@ -10,6 +10,8 @@ public class svm_model implements java.io.Serializable
 	svm_node[][] SV;	// SVs (SV[l])
 	double[][] sv_coef;	// coefficients for SVs in decision functions (sv_coef[n-1][l])
 	double[] rho;		// constants in decision functions (rho[n*(n-1)/2])
+	double[] probA;         // pariwise probability information
+	double[] probB;
 
 	// for classification only
 
