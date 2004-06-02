f7ab3bc268dfcdb3133fbc96c029f0b68c6c1702
cjlin <cjlin@c1691e87-3399-dd11-a7e4-0017319806e7>
2004-06-02 11:17:10 +0000

    setting for use on windows

--- python/easy.py
+++ python/easy.py
@@ -7,6 +7,25 @@ if len(sys.argv) <= 1:
 	print 'Usage: %s training_file [testing_file]' % sys.argv[0]
 	raise SystemExit
 
+# svm, grid, and gnuplot executable
+svmscale_exe = "../svm-scale"
+svmtrain_exe = "../svm-train"
+svmpredict_exe = "../svm-predict"
+grid_py = "./grid.py"
+gnuplot_exe = "/usr/bin/gnuplot"
+# example for windows
+#svmscale_exe = r"c:\tmp\libsvm-2.7\windows\svmscale.exe"
+#svmtrain_exe = r"c:\tmp\libsvm-2.7\windows\svmtrain.exe"
+#svmpredict_exe = r"c:\tmp\libsvm-2.7\windows\svmpredict.exe"
+#gnuplot_exe = r"c:\tmp\gp373w32\pgnuplot.exe"
+#grid_py = r".\grid.py"
+
+assert os.path.exists(svmscale_exe),"svm-scale executable not found"
+assert os.path.exists(svmtrain_exe),"svm-train executable not found"
+assert os.path.exists(svmpredict_exe),"svm-predict executable not found"
+assert os.path.exists(gnuplot_exe),"gnuplot executable not found"
+assert os.path.exists(grid_py),"grid.py not found"
+
 train_pathname = sys.argv[1]
 assert os.path.exists(train_pathname),"training file not found"
 file_name = os.path.split(train_pathname)[1]
@@ -21,11 +40,11 @@ if len(sys.argv) > 2:
 	scaled_test_file = file_name + ".scale"
 	predict_test_file = file_name + ".predict"
 
-cmd = "../svm-scale -s %s %s > %s" % (range_file, train_pathname, scaled_file)
+cmd = "%s -s %s %s > %s" % (svmscale_exe, range_file, train_pathname, scaled_file)
 print 'Scaling training data...'
 os.system(cmd)
 
-cmd = "./grid.py %s" % (scaled_file)
+cmd = "%s -svmtrain %s -gnuplot %s %s" % (grid_py, svmtrain_exe, gnuplot_exe, scaled_file)
 print 'Cross validation...'
 dummy, f = os.popen2(cmd)
 
@@ -38,17 +57,17 @@ c,g,rate = map(float,last_line.split())
 
 print 'Best c=%s, g=%s CV rate=%s' % (c,g,rate)
 
-cmd = "../svm-train -c %s -g %s %s %s" % (c,g,scaled_file,model_file)
+cmd = "%s -c %s -g %s %s %s" % (svmtrain_exe,c,g,scaled_file,model_file)
 print 'Training...'
 os.popen(cmd)
 
 print 'Output model: %s' % model_file
 if len(sys.argv) > 2:
-	cmd = "../svm-scale -r %s %s > %s" % (range_file, test_pathname, scaled_test_file)
+	cmd = "%s -r %s %s > %s" % (svmscale_exe, range_file, test_pathname, scaled_test_file)
 	print 'Scaling testing data...'
 	os.system(cmd)
 
-	cmd = "../svm-predict %s %s %s" % (scaled_test_file, model_file, predict_test_file)
+	cmd = "%s %s %s %s" % (svmpredict_exe, scaled_test_file, model_file, predict_test_file)
 	print 'Testing...'
 	os.system(cmd)
 
