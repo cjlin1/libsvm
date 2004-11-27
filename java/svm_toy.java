d4b1cd432794fee50d90ea04d26edd643e4e4048
jo <jo@c1691e87-3399-dd11-a7e4-0017319806e7>
2004-11-27 10:16:28 +0000

    make "x" button work.

--- java/svm_toy.java
+++ java/svm_toy.java
@@ -441,6 +441,11 @@ class AppletFrame extends Frame {
 	AppletFrame(String title, Applet applet, int width, int height)
 	{
 		super(title);
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
 		applet.init();
 		applet.resize(width,height);
 		applet.start();
