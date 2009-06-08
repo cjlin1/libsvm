41e19d5c8e9df10e9685ba02e668de9f5543101f
cjlin <cjlin@c1691e87-3399-dd11-a7e4-0017319806e7>
2009-06-08 15:01:25 +0000

    use 2.9 instead 2.90

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup, Extension
 
 setup(name = "LIBSVM",
-      version = "2.90",
+      version = "2.9",
       author="Chih-Chung Chang and Chih-Jen Lin",
       maintainer="Chih-Jen Lin",
       maintainer_email="cjlin@csie.ntu.edu.tw",
