868bc32aeeb1efdae9d3e8285ad3535d002e9b36
coha <coha@c1691e87-3399-dd11-a7e4-0017319806e7>
2011-09-01 15:42:26 +0000

    Using try and catch method to show recommend message of make.m Modify README for new MATLAB make.m

--- matlab/make.m
+++ matlab/make.m
@@ -1,18 +1,21 @@
 % This make.m is for MATLAB and OCTAVE under Windows, Mac, and Unix
 
-Type = ver;
-% This part is for OCTAVE
-if(strcmp(Type(1).Name, 'Octave') == 1)
-    mex libsvmread.c
-    mex libsvmwrite.c
-    mex svmtrain.c ../svm.cpp svm_model_matlab.c
-    mex svmpredict.c ../svm.cpp svm_model_matlab.c
-% This part is for MATLAB
-% Add -largeArrayDims on 64-bit machines of MATLAB
-else
-    mex -O CFLAGS="\$CFLAGS -std=c99" -largeArrayDims libsvmread.c
-    mex -O CFLAGS="\$CFLAGS -std=c99" -largeArrayDims libsvmwrite.c
-    mex -O CFLAGS="\$CFLAGS -std=c99" -largeArrayDims svmtrain.c ../svm.cpp svm_model_matlab.c
-    mex -O CFLAGS="\$CFLAGS -std=c99" -largeArrayDims svmpredict.c ../svm.cpp svm_model_matlab.c
+try
+	Type = ver;
+	% This part is for OCTAVE
+	if(strcmp(Type(1).Name, 'Octave') == 1)
+		mex libsvmread.c
+		mex libsvmwrite.c
+		mex svmtrain.c ../svm.cpp svm_model_matlab.c
+		mex svmpredict.c ../svm.cpp svm_model_matlab.c
+	% This part is for MATLAB
+	% Add -largeArrayDims on 64-bit machines of MATLAB
+	else
+		mex -O CFLAGS="\$CFLAGS -std=c99" -largeArrayDims libsvmread.c
+		mex -O CFLAGS="\$CFLAGS -std=c99" -largeArrayDims libsvmwrite.c
+		mex -O CFLAGS="\$CFLAGS -std=c99" -largeArrayDims svmtrain.c ../svm.cpp svm_model_matlab.c
+		mex -O CFLAGS="\$CFLAGS -std=c99" -largeArrayDims svmpredict.c ../svm.cpp svm_model_matlab.c
+	end
+catch
+	fprintf('If make.m failes, please check README about detailed instructions.\n');
 end
-
