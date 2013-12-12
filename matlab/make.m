7e33616e6c8c7c3062347dc6cb01649f68fb6de7
Carnë Draug <carandraug+dev@gmail.com>
2013-12-12 15:34:45 +0000

    Matlab: include 'svm.h' instead of '../svm.h'
    The Matlab Makefile already has .. on the -I option for the compiler
    so this was not required.  This was only required for the make.m Matlab
    and Octave script, so it was modified to include .. on the search path.
    
    This change is required to build the mex files using a libsvm library
    different than the one in this distribution.  Maybe a system-wide install
    of libsvm, or just another version for testing.

--- matlab/make.m
+++ matlab/make.m
@@ -6,15 +6,15 @@ try
 	if(strcmp(Type(1).Name, 'Octave') == 1)
 		mex libsvmread.c
 		mex libsvmwrite.c
-		mex svmtrain.c ../svm.cpp svm_model_matlab.c
-		mex svmpredict.c ../svm.cpp svm_model_matlab.c
+		mex -I.. svmtrain.c ../svm.cpp svm_model_matlab.c
+		mex -I.. svmpredict.c ../svm.cpp svm_model_matlab.c
 	% This part is for MATLAB
 	% Add -largeArrayDims on 64-bit machines of MATLAB
 	else
 		mex CFLAGS="\$CFLAGS -std=c99" -largeArrayDims libsvmread.c
 		mex CFLAGS="\$CFLAGS -std=c99" -largeArrayDims libsvmwrite.c
-		mex CFLAGS="\$CFLAGS -std=c99" -largeArrayDims svmtrain.c ../svm.cpp svm_model_matlab.c
-		mex CFLAGS="\$CFLAGS -std=c99" -largeArrayDims svmpredict.c ../svm.cpp svm_model_matlab.c
+		mex CFLAGS="\$CFLAGS -std=c99" -I.. -largeArrayDims svmtrain.c ../svm.cpp svm_model_matlab.c
+		mex CFLAGS="\$CFLAGS -std=c99" -I.. -largeArrayDims svmpredict.c ../svm.cpp svm_model_matlab.c
 	end
 catch
 	fprintf('If make.m fails, please check README about detailed instructions.\n');
