e34e317d4fa3fcc7c131b7904ba1d554f4e35861
Erich Schubert <erich.schubert@tu-dortmund.de>
2019-08-19 14:38:29 +0200

    Improve syncronization of Java source with C source

--- java/svm_predict.java
+++ java/svm_predict.java
@@ -38,7 +38,7 @@ class svm_predict {
 		int correct = 0;
 		int total = 0;
 		double error = 0;
-		double sumv = 0, sumy = 0, sumvv = 0, sumyy = 0, sumvy = 0;
+		double sump = 0, sumt = 0, sumpp = 0, sumtt = 0, sumpt = 0;
 
 		int svm_type=svm.svm_get_svm_type(model);
 		int nr_class=svm.svm_get_nr_class(model);
@@ -69,7 +69,7 @@ class svm_predict {
 
 			StringTokenizer st = new StringTokenizer(line," \t\n\r\f:");
 
-			double target = atof(st.nextToken());
+			double target_label = atof(st.nextToken());
 			int m = st.countTokens()/2;
 			svm_node[] x = new svm_node[m];
 			for(int j=0;j<m;j++)
@@ -79,29 +79,29 @@ class svm_predict {
 				x[j].value = atof(st.nextToken());
 			}
 
-			double v;
+			double predict_label;
 			if (predict_probability==1 && (svm_type==svm_parameter.C_SVC || svm_type==svm_parameter.NU_SVC))
 			{
-				v = svm.svm_predict_probability(model,x,prob_estimates);
-				output.writeBytes(v+" ");
+				predict_label = svm.svm_predict_probability(model,x,prob_estimates);
+				output.writeBytes(predict_label+" ");
 				for(int j=0;j<nr_class;j++)
 					output.writeBytes(prob_estimates[j]+" ");
 				output.writeBytes("\n");
 			}
 			else
 			{
-				v = svm.svm_predict(model,x);
-				output.writeBytes(v+"\n");
+				predict_label = svm.svm_predict(model,x);
+				output.writeBytes(predict_label+"\n");
 			}
 
-			if(v == target)
+			if(predict_label == target_label)
 				++correct;
-			error += (v-target)*(v-target);
-			sumv += v;
-			sumy += target;
-			sumvv += v*v;
-			sumyy += target*target;
-			sumvy += v*target;
+			error += (predict_label-target_label)*(predict_label-target_label);
+			sump += predict_label;
+			sumt += target_label;
+			sumpp += predict_label*predict_label;
+			sumtt += target_label*target_label;
+			sumpt += predict_label*target_label;
 			++total;
 		}
 		if(svm_type == svm_parameter.EPSILON_SVR ||
@@ -109,8 +109,8 @@ class svm_predict {
 		{
 			svm_predict.info("Mean squared error = "+error/total+" (regression)\n");
 			svm_predict.info("Squared correlation coefficient = "+
-				 ((total*sumvy-sumv*sumy)*(total*sumvy-sumv*sumy))/
-				 ((total*sumvv-sumv*sumv)*(total*sumyy-sumy*sumy))+
+				 ((total*sumpt-sump*sumt)*(total*sumpt-sump*sumt))/
+				 ((total*sumpp-sump*sump)*(total*sumtt-sumt*sumt))+
 				 " (regression)\n");
 		}
 		else
