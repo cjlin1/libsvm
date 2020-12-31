3bdbb8ada4b24e4cd5139613f5089cc33dd5caed
Sheng-Wei Chen <will945945945@gmail.com>
2020-12-31 09:21:55 +0800

    Change the CFLAGS setting ` CFLAGS="\$CFLAGS -std=c99" ' to default setting in the mex functions, that is, we remove it in the mex functions.
    We have tested this change with the environments `Visual Studio 2019' and `MinGW' in `Windows 10',
    `GCC/G++' in `Linux'.

--- matlab/make.m
+++ matlab/make.m
@@ -10,10 +10,10 @@ try
 	% This part is for MATLAB
 	% Add -largeArrayDims on 64-bit machines of MATLAB
 	else
-		mex CFLAGS="\$CFLAGS -std=c99" -largeArrayDims libsvmread.c
-		mex CFLAGS="\$CFLAGS -std=c99" -largeArrayDims libsvmwrite.c
-		mex CFLAGS="\$CFLAGS -std=c99" -I.. -largeArrayDims svmtrain.c ../svm.cpp svm_model_matlab.c
-		mex CFLAGS="\$CFLAGS -std=c99" -I.. -largeArrayDims svmpredict.c ../svm.cpp svm_model_matlab.c
+		mex -largeArrayDims libsvmread.c
+		mex -largeArrayDims libsvmwrite.c
+		mex -I.. -largeArrayDims svmtrain.c ../svm.cpp svm_model_matlab.c
+		mex -I.. -largeArrayDims svmpredict.c ../svm.cpp svm_model_matlab.c
 	end
 catch err
 	fprintf('Error: %s failed (line %d)\n', err.stack(1).file, err.stack(1).line);
