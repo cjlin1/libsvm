2cbc8d5e82a0e24b11adbe7652168832732e8a11
Sinacam <stinkingmadgod@gmail.com>
2022-09-04 23:27:43 +0800

    pypi use openmp by default

--- python/setup.py
+++ python/setup.py
@@ -43,6 +43,14 @@ if sys.platform == "win32":
         {
             "define_macros": [("_WIN64", ""), ("_CRT_SECURE_NO_DEPRECATE", "")],
             "extra_link_args": ["-DEF:{}\svm.def".format(cpp_dir)],
+            "extra_compile_args": ["/openmp"],
+        }
+    )
+else:
+    kwargs_for_extension.update(
+        {
+            "extra_compile_args": ["-fopenmp"],
+            "extra_link_args": ["-fopenmp"],
         }
     )
 
